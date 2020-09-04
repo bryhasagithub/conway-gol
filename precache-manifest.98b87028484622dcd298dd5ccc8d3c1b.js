3080e70c42fde40538941d8f4c00bdb6a7f647e5
Bry Nguyen <brynguyen1331@gmail.com>
2020-09-04 12:48:22 -0700

    Updates

---
+++ precache-manifest.98b87028484622dcd298dd5ccc8d3c1b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60c3ecd48b681e2a4fff68bda280ad28",
+    "url": "/conway-gol/index.html"
+  },
+  {
+    "revision": "15d6404410c364165976",
+    "url": "/conway-gol/static/css/main.050e8e65.chunk.css"
+  },
+  {
+    "revision": "1f029c71c7b49e0ef863",
+    "url": "/conway-gol/static/js/2.a99e79a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/conway-gol/static/js/2.a99e79a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15d6404410c364165976",
+    "url": "/conway-gol/static/js/main.c8e9f194.chunk.js"
+  },
+  {
+    "revision": "365f8292dac9a1f590a2",
+    "url": "/conway-gol/static/js/runtime-main.b1216ce1.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/conway-gol/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/conway-gol/static/media/Montserrat-Regular.ee653992.ttf"
+  }
+]);
